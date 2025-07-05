72c043a8ec387d6b6515b810c340d858bab7e709
Hannah Luebbering <61786322+hluebbering@users.noreply.github.com>
2025-07-05 06:57:56 +0000

    Update devcontainer configuration and add initial Dagster definitions

--- nba_engine/__init__.py
+++ nba_engine/__init__.py
@@ -4,3 +4,6 @@ from . import assets  # still nba_engine/assets/*.py
 defs = Definitions(
     assets=load_assets_from_modules([assets]),
 )
+#from google.cloud import bigquery
+#client = bigquery.Client()
+#print(client.project)   # should print your GCP project ID
